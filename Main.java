de8c4f79c4d27008f5ad12162053b18484caf2ba
mayur2457 <167673706+mayur2457@users.noreply.github.com>
2024-04-21 13:14:13 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.net.StandardSocketOptions;
+
+public class Main {
+    public static void printhelloworld(){
+        System.out.println("hello World");
+
+    }
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");
+        printhelloworld();
+        printhelloworld();
+        printhelloworld();
+        printhelloworld();
+
+    }
+}
